300dcf06f9c172e29d8b3b6b44da37052a30eac4
JGodwin96 <exogenesis96@gmail.com>
2018-02-06 11:10:22 +0000

    Added anagram game

---
+++ Assets/WM2000/Hacker.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+    int level;
+    enum Screen { MainMenu, Password, Win};
+    Screen currentScreen;
+    string[] level1Passwords = { "books", "aisle", "self", "password", "font", "borrow" };
+    string[] level2Passwords = { "prisoner", "handcuffs", "holster", "uniform", "arrest" };
+    string[] level3Passwords = { "starfield", "telescope", "environment", "exploration", "astronauts" };
+
+
+    string password = "";
+    // Use this for initialization
+	void Start () {
+        ShowMainMenu();
+    }
+	
+    void ShowMainMenu () {
+        currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Press 3 for NASA");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter your selection:");
+    }
+
+    void OnUserInput (string input)
+    {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+        else if (currentScreen == Screen.Password)
+        {
+            AttemptPassword(input);
+        }
+    }
+
+    void AttemptPassword(string input)
+    {
+
+        if (input == password)
+        {
+            DisplayWin();
+        }
+        else
+        {
+            Terminal.WriteLine("Invalid password, try again.");
+        }
+    }
+
+    void DisplayWin()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Congratulations, you guessed the password.");
+        Terminal.WriteLine("Type \"menu\" to return to the main menu.");
+    }
+
+    void RunMainMenu(string input)
+    {
+        bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
+        if (isValidLevelNumber)
+        {
+            level = int.Parse(input);
+            StartGame();
+        }
+        else
+        {
+            print("Please choose a valid level.");
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.ClearScreen();
+        switch(level)
+        {
+            case 1:
+                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
+                break;
+            case 2:
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
+                break;
+            case 3:
+                password = level3Passwords[Random.Range(0, level3Passwords.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid level number in StartGame");
+                break;
+        }
+        Terminal.WriteLine("Please enter your password, hint: " + password.Anagram());
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
